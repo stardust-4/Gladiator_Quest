dd53495457303e840e7c2a09b8628b044e35bb03
John <jboyle15@yahoo.com>
2022-12-08 09:17:12 -0600

    fleshed out arena a bit, aadded squa minimap thingy

--- gladiatorquest/src/assets/backgrounds.js
+++ gladiatorquest/src/assets/backgrounds.js
@@ -1,5 +1,8 @@
 const backgrounds = [
-  { name: '1', url: 'gladiatorquest/src/assets/game_background_1.png' },
+  {
+    name: '1',
+    url: 'https://ucarecdn.com/c92f5a09-3e44-4ed1-878b-c376fee09220/'
+  },
   { name: '2', url: 'https://i.imgur.com/7RvwMJZ.png' },
   { name: '3', url: 'https://i.imgur.com/MBF6BjS.png' },
   { name: '4', url: 'https://i.imgur.com/k46gwRb.png' }
